55aa2926289aa75509e1e1b549dadaa74d4048c7
Florian Goerke <florian@goerke.de>
2024-04-23 19:23:28 +0200

    Initial commit

---
+++ src/main/java/de/gurkinator/food_and_power/Food_and_power.java
@@ -0,0 +1,17 @@
+package de.gurkinator.food_and_power;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Food_and_power extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
